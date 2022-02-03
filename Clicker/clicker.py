e3a7299af85ed7a831117a4c3733359d1038db52
JayPip <46865973+JayPip@users.noreply.github.com>
2022-02-03 15:48:18 +0100

    Add files via upload

--- Clicker/clicker.py
+++ Clicker/clicker.py
@@ -1,10 +1,14 @@
 
 from faulthandler import disable
+from os import stat
 from tkinter import *
 from tkinter import messagebox
 import time
 from threading import *
 from tkinter import ttk
+from tempfile import mkstemp
+from shutil import move, copymode
+from os import fdopen, remove
 
 form = Tk()
 
@@ -38,6 +42,8 @@ def infoPopup():
     elif pace>=7:
         messagebox.showinfo("Your Pace", "Global Elite")
         reset()
+    updateData(pace)
+    getData()
 
 def startCount():   
     timerLabel.set('3!')
@@ -53,6 +59,7 @@ def startCount():
     Clickerbtn['state'] = "normal"
     Clickerbtn['bg'] = "green"
     form.after(5000, infoPopup)
+    
 
 def timerThread():
     thread = Thread(target = startCount)
@@ -60,7 +67,6 @@ def timerThread():
 
 
 def getData():
-#    updateData()
     f = open("scores.txt", "r")
     fileData = f.readlines()
     f.close()
@@ -69,16 +75,39 @@ def getData():
         subset = set.split(" ")
         treeView.insert("", 'end',  
              values =(subset[0], subset[1]))
-    form.after(300, getData)
     
     
-def updateData():
+def updateData(pace): 
     f = open("scores.txt", "r")
     fileData = f.readlines()
+    x = True
     for set in fileData:
         subset = set.split(" ")
         if subset[0]== userName:
-            subset[1]=ctr/5
+            pattern = set
+            x = False
+            global ctr
+            print(pace)
+            subst = subset[0] + " " + str(pace) +"\n"
+    f.close()
+    if x == True:
+        f = open("scores.txt", "a")
+        f.write(userName + " " + str(pace)+ "\n")
+        f.close()
+    else:
+        #Create temp file
+        fh, abs_path = mkstemp()
+        with fdopen(fh,'w') as new_file:
+            with open("scores.txt") as old_file:
+                for line in old_file:
+                    new_file.write(line.replace(pattern, subst))
+        #Copy the file permissions from the old file to the new file
+        copymode("scores.txt", abs_path)
+        #Remove original file
+        remove("scores.txt")
+        #Move new file
+        move(abs_path, "scores.txt")
+    
 
 def listThread():
     thread = Thread(target = getData)
@@ -98,38 +127,31 @@ def welcomeForm():
     textForm = Toplevel(form)
     textForm.title("Enter your Username:")
     textForm.geometry("300x100")
-    
-def LogForm():
-    form.withdraw()
-    LogForm = Toplevel(form)
-    LogForm.title("Your Username")
-    LogForm.geometry("300x100")
-    UserTxt = Text(LogForm, height = 1, width= 15)
-    UserTxt.grid(row = 0, column= 0)
-    Savebtn = Button(LogForm, text = "Save", command = saveName(UserTxt))
-    CloseBtn = Button(LogForm, text="Close", command = closeLog(LogForm))
-    Savebtn.grid(row = 1, column= 0)
-    CloseBtn.grid(row = 1, column = 1)
 
-def saveName(UserTxt):
+def save():
     global userName
-    userName = UserTxt.get("1.0",END)
+    userName = UserTxt.get("1.0", "end-1c")
+    Startbtn["state"] ="normal"
 
-def closeLog(LogForm):
-    form.deiconify()
-    LogForm.destroy
+
+UserTxt = Text(form, height = 1, width= 15)
+UserTxt.grid(row = 0, column= 0, columnspan=2, sticky=W, pady=15, padx=50)
+Savebtn = Button(form, text = "Save", command =save)
+Savebtn.grid(row = 0, column= 0, columnspan=2, sticky=W, padx=15)
+
+
 
 ctrString = StringVar()
 timerLabel = StringVar()
 ctrString.set("Your Clicks: ")
 
-Startbtn = Button(form, text = "Start your test", command = timerThread)
+Startbtn = Button(form, text = "Start your test", command = timerThread, state="disabled")
 Clickerbtn = Button(form, text = "Click!",bg = "red",
     highlightcolor = "red", width = 20, height= 5, command = counter,
     state= "disabled")
 Timerlbl = Label(form, textvariable= timerLabel) 
 Pacelbl = Label(form, textvariable = ctrString )
-LogBtn = Button(form, text = "Log In", command = LogForm)
+
 
 
 
@@ -144,17 +166,15 @@ treeView.column("2", width = 50, anchor ='c')
 treeView.heading("1", text ="Username") 
 treeView.heading("2", text ="Score") 
 
-LogBtn.grid(row = 0, column= 0, padx=15 )
+
 Startbtn.grid(row = 1, column= 0, padx=15 )
 Timerlbl.grid(row = 2, column= 0 , padx=15)
 Clickerbtn.grid(row = 3, column= 0 , padx=15)
 Pacelbl.grid(row = 4, column= 0 , padx=15 )
 treeView.grid(row = 0, column = 1, sticky= W, rowspan=5, padx=5, pady= 5)
+
 if userName == "":
      Startbtn["state"] = "disabled"
-else:
-    LogBtn["state"] = "disabled"
-    Startbtn["state"] ="enabled"
 
 listThread()
 form.mainloop()
